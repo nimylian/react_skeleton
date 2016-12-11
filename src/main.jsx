bd3922395e0b0351460636bcb04956548bd31834
John Schulz <johnschulz75@gmail.com>
2016-12-10 21:21:11 -0600

    React Skeleton Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const List = require('./components/List.jsx');
+
+ReactDOM.render(<List />, document.getElementById('ingredients'));
